216d71168847d2400e029bc8e9ae998fd56bf3f9
Nick Hoffman <nhoff@udel.edu>
2016-11-09 23:51:13 -0500

    Start screen to play with

--- src/game/StartScreen.java
+++ src/game/StartScreen.java
@@ -2,8 +2,10 @@ package game;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
+import java.awt.Component;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -11,7 +13,10 @@ import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 
 import view.MainView;
 
@@ -33,11 +38,21 @@ public class StartScreen extends JPanel{
 	}
 	
 	public StartScreen(Game game){
-		setBackground(Color.BLACK);
-		setOpaque(false);
-		setBorder(BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Crabby"));
+		this.setLayout(new GridLayout(8,1));
+		setBackground(Color.CYAN);
+		setOpaque(true);
+		TitledBorder title = BorderFactory.createTitledBorder(BorderFactory.createEtchedBorder(), "Game1");
+		title.setTitleJustification(TitledBorder.CENTER);
+		setBorder(title);
 		this.add(start);
 		start.addActionListener(new Starter(game));
+		this.add(new JLabel("Estuary Game"));
+		this.add(new JLabel("Instructions:"));
+		this.add(new JLabel("Left/right to move"));
+		this.add(new JLabel("Up to jump"));
+		this.add(new JLabel("Collect trash bags, hay, compost, and seeds."));
+		this.add(new JLabel("Use on enemies with "));
+		this.add(new JLabel("Don't fall off!"));
 	}
 	
 	class Starter implements ActionListener{
