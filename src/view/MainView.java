1d4f265d8f57fe3bda9d0965b44d6d37725c3bb3
kblack789 <kblacks@udel.edu>
2016-11-15 22:04:48 -0500

    Add files via upload

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -1,52 +1,30 @@
 package view;
 
 import java.awt.Color;
-import java.awt.Dimension;
 import java.awt.Graphics;
 import javax.swing.JComponent;
-import javax.swing.JLabel;
 
 import game.Game;
 
 public class MainView extends JComponent{ //extend canvas and use bufferedimage here?
 	//private Game game;
 	//private GameController gc = game.gameControl;
-	private JLabel start = new JLabel("START!!");
 	
 	public MainView() {
 		
 	}
 	
-	@Override
 	public void paintComponent(Graphics g){
 		super.paintComponent(g);
 		g.setColor(Color.RED);
 		g.setColor(Color.CYAN);
 		g.fillRect(0, 0, getWidth(), getHeight());
-//		start.setOpaque(true);
-//		add(start);
-//		start.setOpaque(true);
 		Game.gameControl.draw(g);
-		//add(new label());
 	}
 
 	public void update() {
 		Game.gameControl.update();
-		start.repaint();
 	}
 	
-	class label extends JLabel{
-		public label(){
-		setPreferredSize(new Dimension(40,50));
-		setOpaque(true);
-		repaint();
-		}
-		@Override 
-		public void paintComponent(Graphics g){
-			super.paintComponent(g);
-			g.setColor(Color.BLACK);
-			g.fillRect(0, 0, 40, 50);
-		}
-	}
 	
 }
