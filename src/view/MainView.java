9e8df80855997f9c6c71d989d3d02a81e048dd57
zionaranda <zaranda@udel.edu>
2016-11-17 02:10:09 -0500

    Beta

--- src/view/MainView.java
+++ src/view/MainView.java
@@ -5,8 +5,12 @@ import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.Image;
 import java.awt.LayoutManager;
+import java.io.File;
+import java.io.IOException;
 
+import javax.imageio.ImageIO;
 import javax.swing.BoxLayout;
 import javax.swing.JComponent;
 import javax.swing.JFrame;
@@ -23,23 +27,46 @@ public class MainView extends JComponent{ //extend canvas and use bufferedimage 
 	//private GameController gc = game.gameControl;
 	//JFrame frame = new JFrame();
 	public Status statusPanel = new Status();
+	Image background;
 	public MainView() {
-		
-	}
-	
-	public void paintComponent(Graphics g){
-		super.paintComponent(g);
-		//Game.setVisible(true);s
-		//g.setColor(Color.RED);
-		g.setColor(Color.CYAN);
-		g.fillRect(0, 0, getWidth(), getHeight());
-		Game.getPane().add(statusPanel, BorderLayout.SOUTH);
-		Game.gameControl.draw(g);
+
 	}
 
-	public void update() {
-		Game.gameControl.update();
+	public void paintComponent(Graphics g){
+		int curr_level = Game.getLevel();
+		switch(curr_level){
+				case 1:
+				background = createImage("Final Images/Backgrounds/underwater2.png");
+				break;
+				case 2:
+				background = createImage("Final Images/Backgrounds/lvltwobg.png");
+				break;
+				case 3:
+				background = createImage("Final Images/Backgrounds/2D_estuary.jpg");
+				break;
+		}
+	super.paintComponent(g);
+	//Game.setVisible(true);s
+	//g.setColor(Color.RED);
+	// g.setColor(Color.CYAN);
+	// g.fillRect(0, 0, getWidth(), getHeight());
+	g.drawImage(background, 0, 0, getWidth(), getHeight(), null);
+	Game.getPane().add(statusPanel, BorderLayout.SOUTH);
+	Game.gameControl.draw(g);
 	}
-	
-	
+	public void update() {
+	Game.gameControl.update();
+	}
+
+	private Image createImage(String file) {
+		Image img;
+		try{
+			img = ImageIO.read(new File(file));
+			return img;
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
 }
