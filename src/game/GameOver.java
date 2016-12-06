1bd4161809d167aafc1a56b56af658616a06a43b
zionaranda <zaranda@udel.edu>
2016-12-05 19:44:38 -0500

    Intermediary screens functionality added

---
+++ src/game/GameOver.java
@@ -0,0 +1,61 @@
+package game;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Rectangle;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class GameOver extends JPanel {
+
+	public Rectangle playButton = new Rectangle(Game.WIDTH *2 - 50, 150, 100, 50);
+	public Rectangle quitButton = new Rectangle(Game.WIDTH *2 - 50, 350, 100, 50);
+
+	public GameOver(Game game){
+	}
+	
+	public void paintComponent(Graphics g){
+		Graphics2D g2d = (Graphics2D) g;
+		g.drawImage(createImage("Final Images/Backgrounds/Estuary_Background_2_Bad.jpg"),0,0,Game.WIDTH*4,Game.HEIGHT*4, Color.BLACK,null);
+		g.drawImage(createImage("Final Images/UI Buttons, Icons, Symbols/smallButton.png"), playButton.x, playButton.y, 100, 50, Color.BLUE, null);
+		g.drawImage(createImage("Final Images/UI Buttons, Icons, Symbols/smallButton.png"), quitButton.x, quitButton.y, 100, 50, Color.BLUE, null);
+	
+		
+		Font fnt0 = new Font("arial", Font.BOLD, 50);
+		g.setFont(fnt0);
+		g.setColor(Color.white);
+		g.drawString("GAME OVER", 350, 100);
+		
+		Font fnt1 = new Font("arial", Font.BOLD, 30);
+		g.setFont(fnt1);
+		g.drawString("Play", playButton.x + 19, playButton.y + 35);
+//		g2d.draw(playButton);
+		g.drawString("Quit", quitButton.x + 19, quitButton.y + 35);
+//		g2d.draw(quitButton);
+	}
+	
+	/**
+	 * Attempts to load an image given the filename.
+	 * @param file
+	 * @return img
+	 */
+	private Image createImage(String file) {
+		Image img;
+		try{
+			img = ImageIO.read(new File(file));
+			return img;
+		}
+		catch (IOException e){
+			e.printStackTrace();
+		}
+		return null;
+	}
+}
